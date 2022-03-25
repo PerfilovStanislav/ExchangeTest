3d7923202085a9a38d799782fcf0c188e9d3e06e
stas <stanislav.perfilov@gmail.com>
2022-03-25 22:12:24 +0300

    fix

--- candles.go
+++ candles.go
@@ -162,13 +162,6 @@ func (data *CandleData) CalculateSma(n, i int, barType string) float64 {
 	return calc
 }
 
-func (data *CandleData) calculateEma(n, i int, barType string) float64 {
-	if i > 0 {
-		return (data.Candles[barType][i]*float64(n) + float64(100-n)*data.getEma(n, i-1, barType)) * 0.01
-	}
-	return data.Candles[barType][i]
-}
-
 func (data *CandleData) calculateDema(n, i int, barType string) float64 {
 	return 2*data.getEma(n, i, barType) - data.get2Ema(n, i, barType)
 }
@@ -177,37 +170,9 @@ func (data *CandleData) calculateTema(n, i int, barType string) float64 {
 	return 3*(data.getEma(n, i, barType)-data.get2Ema(n, i, barType)) + data.get3Ema(n, i, barType)
 }
 
-func (data *CandleData) calculate2Ema(n, i int, barType string) float64 {
+func (data *CandleData) calculateEma(source, prev funGet, n, i int, barType string) float64 {
 	if i > 0 {
-		return (data.getEma(n, i, barType)*float64(n) + float64(100-n)*data.get2Ema(n, i-1, barType)) * 0.01
-	}
-	return data.Candles[barType][i]
-}
-
-func (data *CandleData) calculate3Ema(n, i int, barType string) float64 {
-	if i > 0 {
-		return (data.get2Ema(n, i, barType)*float64(n) + float64(100-n)*data.get3Ema(n, i-1, barType)) * 0.01
-	}
-	return data.Candles[barType][i]
-}
-
-func (data *CandleData) calculateEmaTema(n, i int, barType string) float64 {
-	if i > 0 {
-		return (data.getTema(n, i, barType)*float64(n) + float64(100-n)*data.getEmaTema(n, i-1, barType)) * 0.01
-	}
-	return data.Candles[barType][i]
-}
-
-func (data *CandleData) calculate2EmaTema(n, i int, barType string) float64 {
-	if i > 0 {
-		return (data.getEmaTema(n, i, barType)*float64(n) + float64(100-n)*data.get2EmaTema(n, i-1, barType)) * 0.01
-	}
-	return data.Candles[barType][i]
-}
-
-func (data *CandleData) calculate3EmaTema(n, i int, barType string) float64 {
-	if i > 0 {
-		return (data.get2EmaTema(n, i, barType)*float64(n) + float64(100-n)*data.get3EmaTema(n, i-1, barType)) * 0.01
+		return (source(n, i, barType)*float64(n) + float64(100-n)*prev(n, i-1, barType)) * 0.01
 	}
 	return data.Candles[barType][i]
 }
@@ -220,10 +185,15 @@ func (data *CandleData) calculateTemaZero(n, i int, barType string) float64 {
 	return 2*data.getTema(n, i, barType) - data.get2Tema(n, i, barType)
 }
 
-type indicatorGet func(n, i int, barType string) float64
+type funGet func(n, i int, barType string) float64
+type funEma func(source, prev funGet, n, i int, barType string) float64
 
 // GET
-func (data *CandleData) get(indicatorType IndicatorType, fun indicatorGet, n, i int, barType string) float64 {
+func (data *CandleData) getCandle(n, i int, barType string) float64 {
+	return data.Candles[barType][i]
+}
+
+func (data *CandleData) get(indicatorType IndicatorType, fun funGet, n, i int, barType string) float64 {
 	arr := data.Indicators[indicatorType][n][barType]
 	if len(arr) > i {
 		return arr[i]
@@ -237,16 +207,30 @@ func (data *CandleData) get(indicatorType IndicatorType, fun indicatorGet, n, i 
 	return arr[i]
 }
 
+func (data *CandleData) ema(indicatorType IndicatorType, fun funEma, source, prev funGet, n, i int, barType string) float64 {
+	arr := data.Indicators[indicatorType][n][barType]
+	if len(arr) > i {
+		return arr[i]
+	}
+
+	for k := len(arr); k <= i; k++ {
+		arr = append(arr, fun(source, prev, n, k, barType))
+		data.Indicators[indicatorType][n][barType] = arr
+	}
+
+	return arr[i]
+}
+
 func (data *CandleData) getEma(n, i int, barType string) float64 {
-	return data.get(IndicatorTypeEma, data.calculateEma, n, i, barType)
+	return data.ema(IndicatorTypeEma, data.calculateEma, data.getCandle, data.getEma, n, i, barType)
 }
 
 func (data *CandleData) get2Ema(n, i int, barType string) float64 {
-	return data.get(IndicatorType2Ema, data.calculate2Ema, n, i, barType)
+	return data.ema(IndicatorType2Ema, data.calculateEma, data.getEma, data.get2Ema, n, i, barType)
 }
 
 func (data *CandleData) get3Ema(n, i int, barType string) float64 {
-	return data.get(IndicatorType3Ema, data.calculate3Ema, n, i, barType)
+	return data.ema(IndicatorType3Ema, data.calculateEma, data.get2Ema, data.get3Ema, n, i, barType)
 }
 
 func (data *CandleData) getDema(n, i int, barType string) float64 {
@@ -258,15 +242,15 @@ func (data *CandleData) getTema(n, i int, barType string) float64 {
 }
 
 func (data *CandleData) getEmaTema(n, i int, barType string) float64 {
-	return data.get(IndicatorTypeEmaTema, data.calculateEmaTema, n, i, barType)
+	return data.ema(IndicatorTypeEmaTema, data.calculateEma, data.getTema, data.getEmaTema, n, i, barType)
 }
 
 func (data *CandleData) get2EmaTema(n, i int, barType string) float64 {
-	return data.get(IndicatorType2EmaTema, data.calculate2EmaTema, n, i, barType)
+	return data.ema(IndicatorType2EmaTema, data.calculateEma, data.getEmaTema, data.get2EmaTema, n, i, barType)
 }
 
 func (data *CandleData) get3EmaTema(n, i int, barType string) float64 {
-	return data.get(IndicatorType3EmaTema, data.calculate3EmaTema, n, i, barType)
+	return data.ema(IndicatorType3EmaTema, data.calculateEma, data.get2EmaTema, data.get3EmaTema, n, i, barType)
 }
 
 func (data *CandleData) get2Tema(n, i int, barType string) float64 {
